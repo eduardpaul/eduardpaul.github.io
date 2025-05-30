c8fff8bfdc3778cbb54032893b6fa592cbefaa14
Eduard <eduardpaul@users.noreply.github.com>
2025-05-30 19:16:56 +0200

    base

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Eduard Paul Lakida`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
